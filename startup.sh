d050329a71f536d87b4fcdec4451121c8d90b04d
lo <jason.lo@canonical.com>
2016-08-23 11:48:54 +0800

    verified startup.sh back to execute bash

--- startup.sh
+++ startup.sh
@@ -5,5 +5,5 @@ set -e
 # install davfs2
 apt-get install -y davfs2
 
-#exec /bin/bash
-exec /jenkins-build-script/make-amd64-all-snap.sh
+exec /bin/bash
+#exec /jenkins-build-script/make-amd64-all-snap.sh
